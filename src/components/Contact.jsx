a833d01dc67b6e837c1c860dfd5c486f145be5a7
Brian Adams <brianadamsmaino68@gmail.com>
2023-08-22 16:46:40 +0200

    Almost done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ export const Contact = () => {
       body: JSON.stringify(formDetails),
     });
     setButtonText("Send");
-    let result = response.json();
+    let result = await response.json();
     setFormDetails(formInitialDetails);
     if (result.code === 200) {
         setStatus({success: true, message: "Message sent successfully"});
